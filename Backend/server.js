5c2218c426fed257903c2cdce76af56ba6eb296c
maybenischal <nischaldhakal1409@gmail.com>
2025-07-04 08:34:29 +0545

    feat: add Cloudinary configuration and update server to connect to Cloudinary

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
 import "dotenv/config";
+import connectCloudinary from "./config/cloudinary.js";
 
 //App Config
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 connectDB();
+connectCloudinary();
 
 //API Endpoints
 app.get("/", (req, res) => {
