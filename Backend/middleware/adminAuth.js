2f2d8bfc805e95eb0f363a48a33a5d11061785a4
maybenischal <nischaldhakal1409@gmail.com>
2025-07-10 08:27:42 +0545

    feat: implement admin authentication for product management routes

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -1,20 +1,22 @@
-// const adminAuth = async (req, res, next) => {
-//   try {
-//     const { token } = req.headers;
-//     if (!token) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "No Authorization. Login Again" });
-//     }
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     if (!decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-//       return res
-//         .status(403)
-//         .json({ success: false, message: "Access Denied. Admins Only" });
-//     }
-//   } catch (error) {
-//     console.error("Error in admin authentication:", error);
-//     res.status(500).json({ success: false, message: "Internal Server Error" });
-//   }
-//   next();
-// };
+const adminAuth = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "No Authorization. Login Again" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Access Denied. Admins Only" });
+    }
+    next();
+  } catch (error) {
+    console.error("Error in admin authentication:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export default adminAuth;
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -7,16 +7,18 @@ import {
 } from "../controllers/productController.js";
 
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post(
   "/add",
   upload.fields([{ name: "image", maxCount: 1 }]),
+  adminAuth,
   addProduct
 );
-productRouter.get("/list", listProducts);
-productRouter.post("/remove", removeProduct);
-productRouter.get("/details/:id", productDetails);
+productRouter.get("/list", adminAuth, listProducts);
+productRouter.post("/remove", adminAuth, removeProduct);
+productRouter.get("/details/:id", adminAuth, productDetails);
 
 export default productRouter;
