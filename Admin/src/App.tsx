584b9b6f1d08f002c9f33f168d9cc188d62aedec
maybenischal <nischaldhakal1409@gmail.com>
2025-07-10 08:57:13 +0545

    feat: add routing and pages for product management, including AddProducts and ListProducts

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,10 +1,19 @@
-
+import Sidebar from "./Components/Sidebar"
+import AddProducts from "./pages/AddProducts"
+import ListProducts from "./pages/ListProducts"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <h1>Hi</h1>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<h1>Admin Dashboard</h1>} />
+        <Route path="/addproducts" element={<AddProducts />} />
+        <Route path="/listproducts" element={<ListProducts />} />
+        {/* Add more admin routes as needed */}
+      </Routes>
 
     </>
   )
---
+++ Admin/src/Components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const Sidebar = () => {
+    return (
+        <div className=' w-[18%] flex flex-col border-r-1 gap-4 m-4'>
+            <p>Add Products</p>
+            <p>List Produtcs</p>
+        </div>
+    )
+}
+
+export default Sidebar
